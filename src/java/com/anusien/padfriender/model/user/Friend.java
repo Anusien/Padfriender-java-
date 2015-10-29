623b955ac4bb1bb9a63d514b7bf63282de0e3bbb
Kevin Binswanger <kbinswanger@gmail.com>
2015-10-29 05:54:21 -0500

    Added notes to friends, some spring wiring

--- src/java/com/anusien/padfriender/model/user/Friend.java
+++ src/java/com/anusien/padfriender/model/user/Friend.java
@@ -1,13 +1,16 @@
 package com.anusien.padfriender.model.user;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class Friend {
     @Nonnull private final User user;
     private final boolean bestFriend;
+    @Nullable public String note;
 
-    public Friend(@Nonnull final User user, final boolean bestFriend) {
+    public Friend(@Nonnull final User user, final boolean bestFriend, @Nullable final String note) {
         this.user = user;
         this.bestFriend = bestFriend;
+        this.note = note;
     }
 }
