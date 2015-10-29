a0912bb24a4f30d8cde4d35bbe56802c7e5ca253
Kevin Binswanger <kbinswanger@gmail.com>
2015-10-29 06:11:29 -0500

    Add basic security and login pages

--- src/java/com/anusien/padfriender/web/security/SecurityConfig.java
+++ src/java/com/anusien/padfriender/web/security/SecurityConfig.java
@@ -2,15 +2,28 @@ package com.anusien.padfriender.web.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
+    }
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/web/static/**").permitAll()
+                    .anyRequest().authenticated()
+                .and().formLogin()
+                    .loginPage("/login").permitAll()
+                .and().logout().permitAll();
     }
 }
---
+++ web/WEB-INF/views/login.jsp
@@ -0,0 +1,31 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<jsp:useBean id="_csrf" scope="request" type="org.springframework.security.web.csrf.CsrfAuthenticationStrategy.SaveOnAccessCsrfToken"/>
+<html>
+<head>
+    <title>Padfriender : Login</title>
+</head>
+<body>
+<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>.
+<div content>
+    <form name="f" action="login" method="post">
+        <fieldset>
+            <legend>Please Login</legend>
+            <c:if test="${param.error}">
+                Invalid username or password.
+            </c:if>
+            <c:if test="${param.logout}">
+                You have been logged out.
+            </c:if>
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username"/>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password"/>
+            <div class="form-actions">
+                <button type="submit" class="btn">Log in</button>
+            </div>
+        </fieldset>
+    </form>
+</div>
+</body>
+</html>
