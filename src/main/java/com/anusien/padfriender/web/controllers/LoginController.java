c1b3d8345482c39ee31db541d170d0d7ab8c51c3
Kevin Binswanger <kbinswanger@gmail.com>
2015-10-29 07:23:12 -0500

    More spring glue

--- src/main/java/com/anusien/padfriender/web/controllers/LoginController.java
+++ src/main/java/com/anusien/padfriender/web/controllers/LoginController.java
@@ -3,7 +3,8 @@ package com.anusien.padfriender.web.controllers;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RequestMapping(value = "/login")
@@ -11,8 +12,8 @@ import org.springframework.web.servlet.view.RedirectView;
 public class LoginController {
     private static final Logger LOGGER = Logger.getLogger(LoginController.class);
 
-    @Override
-    public String doGet() {
-        return "login";
+    @RequestMapping(value="", method= RequestMethod.GET)
+    public ModelAndView doGet() {
+        return new ModelAndView("login");
     }
 }
