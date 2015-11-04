3247f4a68f2094b66e98967f8a7d76d4aeacf632
Kevin Binswanger <kbinswanger@gmail.com>
2015-11-04 02:56:31 -0600

    NOBUG This should hash properly

--- src/main/java/com/anusien/padfriender/persistence/user/UserDao.java
+++ src/main/java/com/anusien/padfriender/persistence/user/UserDao.java
@@ -3,14 +3,12 @@ package com.anusien.padfriender.persistence.user;
 import com.anusien.padfriender.model.user.UserId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
 import javax.sql.DataSource;
 import java.io.IOException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 @Component
 public class UserDao {
@@ -27,15 +25,7 @@ public class UserDao {
     // TODO check to see if that user already exists
     public void createUser(@Nonnull final String email, @Nonnull final String name, @Nonnull final UserId friendCode,
                            @Nonnull final String password) throws IOException {
-        final String hash;
-        try {
-            final MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(password.getBytes());
-            final byte[] bytes = md.digest();
-            hash = new BigInteger(1, bytes).toString(16);
-        } catch (NoSuchAlgorithmException e) {
-            throw new IOException(e);
-        }
+        final String hash = new ShaPasswordEncoder(256).encodePassword(password, email);
 
         final JdbcTemplate template = new JdbcTemplate(dataSource);
         template.update(createUserSql, email, name, hash, friendCode.toString(), 1);
